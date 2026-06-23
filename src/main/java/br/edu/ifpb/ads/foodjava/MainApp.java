376774992d435eb66f9110cf339acd33ad2cb313
THAS6 <tiagohas2006@gmail.com>
2026-06-22 21:20:15 -0300

    controle de acesso gerente/cliente

--- src/main/java/br/edu/ifpb/ads/foodjava/MainApp.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/MainApp.java
@@ -1,44 +1,38 @@
 package br.edu.ifpb.ads.foodjava;
 
-import br.edu.ifpb.ads.foodjava.controller.CardapioController;
-import br.edu.ifpb.ads.foodjava.controller.ItemFormController;
-import br.edu.ifpb.ads.foodjava.exception.ArquivoImportacaoException;
+import br.edu.ifpb.ads.foodjava.model.Cliente;
+import br.edu.ifpb.ads.foodjava.model.Gerente;
+import br.edu.ifpb.ads.foodjava.util.Sessao;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-/**
- * Ponto de entrada da aplicação FoodJava.
- *
- * Esta classe deve ser mantida mínima — ela apenas inicializa o JavaFX
- * e carrega a primeira tela. Toda a lógica de negócio deve ficar nos
- * pacotes model, controller e repository.
- *
- * DICA: para carregar uma tela FXML, substitua o conteúdo de start() por:
- *
- *   FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
- *   Parent root = loader.load();
- *   stage.setScene(new Scene(root, 900, 600));
- *   stage.show();
- */
 public class MainApp extends Application {
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
+        try {
 
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/fxml/cardapio.fxml")
-        );
-          Parent root = loader.load();
-          stage.setScene(new Scene(root, 900,600));
-          stage.show();
+            Sessao.iniciar(new Cliente(1, "Joao", "Joao@email.com", "4321", "0011111111", "000.000.000-00", "muito longe"));
+
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/fxml/Cardapio.fxml")
+            );
+
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+
+            stage.setTitle("FoodJava - Cardápio");
+            stage.setScene(scene);
+            stage.setResizable(true);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
